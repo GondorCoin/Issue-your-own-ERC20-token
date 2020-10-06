4532ffc5cc44dc7445ba48e2f52c5f01757d36c4
GondorCoin <72226995+GondorCoin@users.noreply.github.com>
2020-10-06 17:44:31 +0300

    GondorToken
    Yedeğim

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.24;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'Gondor' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
-// Total supply: 100000000
+// Deployed to : 0xB01c29541c30A6b8d342B3BECaB507F2879fc2cC
+// Symbol      : GON
+// Name        : Gondor
+// Total supply: 35000
 // Decimals    : 18
 //
 // Enjoy.
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract GondorToken is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -113,12 +113,12 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     constructor() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
+        symbol = "GON";
+        name = "GONDOR";
         decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        emit Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        _totalSupply = 35000000000000000000000;
+        balances[0xB01c29541c30A6b8d342B3BECaB507F2879fc2cC] = _totalSupply;
+        emit Transfer(address(0), 0xB01c29541c30A6b8d342B3BECaB507F2879fc2cC, _totalSupply);
     }
 
 
